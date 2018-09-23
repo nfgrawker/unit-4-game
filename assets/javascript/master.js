5d207aaef51b8771655532c3a754422d10fd390b
nfgrawker <nfgrawker@gmail.com>
2018-09-23 17:48:45 -0500

    some changes

--- assets/javascript/master.js
+++ assets/javascript/master.js
@@ -4,46 +4,75 @@ var axe = {
   hitpoints:520,
   attackpower:25,
   counter:25,
-  order: ".one"
+  order: ".one",
+  image: "./assets/images/axebackground.jpg"
 }
 var razor = {
   name:"Razor",
   hitpoints:420,
   attackpower:30,
   counter:30,
-  order:".two"
+  order:".two",
+  image: "./assets/images/razorbackground.jpg"
 }
 var wraithking = {
   name:"Wraith King",
   hitpoints:580,
   attackpower:25,
   counter:25,
-  order:".three"
+  order:".three",
+  image: "./assets/images/wraithkingbackground.jpg"
 }
 var invoker = {
   name:"Invoker",
   hitpoints:520,
   attackpower:25,
   counter:25,
-  order:".four"
+  order:".four",
+  image: "./assets/images/invokerbackground.jpg"
 }
 
 var heroes = [axe, razor, wraithking, invoker]
 var userhero = ""
 var enemyheroes = []
+var firstexecute = false
+var firstexecute1 = false
+var currentlyattacking = ""
 
 $(".clicker").on("click", function(){
-  var attribute = $(this).attr("hero")
-  for (i in heroes){
-    if (attribute == (heroes[i]).name){
-      userhero = (heroes[i]).name
+  if (firstexecute == false){
+    firstexecute = true
+    var attribute = $(this).attr("hero")
+    for (i in heroes){
+      if (attribute == (heroes[i]).name){
+        userhero = heroes[i]
+        console.log(userhero)
+        $("body").css("background-image", "url("+userhero.image+")");
+      }
+      else{
+        enemyheroes.push(heroes[i])
+      }
+    }
+
+    for (i in enemyheroes){
+      $("div"+(enemyheroes[i]).order).prependTo(".available")
+      $("div"+(enemyheroes[i]).order).addClass("enemy")
+    }
     }
     else{
-      enemyheroes.push(heroes[i])
+      console.log("already run")
+    }
+})
+$(".enemy").on("click",function(){
+  if (firstexecute1 == false){
+    firstexecute1 = true
+    var current = $(this).attr("hero")
+    for (i in enemyheroes){
+      if (attribute == (enemyheroes[i]).name){
+      currentlyattacking = enemyheroes[i]
+    console.log(currentlyattacking.order)
     }
   }
-  for (i in enemyheroes){
-    $((enemyheroes[i]).order).prependTo(".available")
-  }
+}
 })
 })
